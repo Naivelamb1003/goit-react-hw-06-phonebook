4d5ee371da4e4973692412d14aa0eff22f6258cf
Malik Nadiia <59849318+Naivelamb1003@users.noreply.github.com>
2021-03-24 01:22:32 +0200

    Redux Tookit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension'
+
+import { configureStore } from '@reduxjs/toolkit'
 import phonebookReducer from './phonebook/phonebook-reducer';
 
-const rootReducer = combineReducers({
-    phonebook:  phonebookReducer
 
+
+
+const store = configureStore ({
+    reducer: {
+        phonebook:  phonebookReducer
+    },
+    devTools: process.env.NODE_ENV === 'development', 
 })
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
